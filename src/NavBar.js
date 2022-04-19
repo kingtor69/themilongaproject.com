42fee274e25986f4915c89d7464d3e4962e39ada
Tor Kingdon <tor@hearkitty.com>
2022-04-19 13:03:37 -0600

    strategy meeting

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+// home (top)
+// about the project and/or process (text coming from Julie)
+// cast and crew - credits (embedded video)
+// behind the scenes
+// about the principles
+  // now(?): 
+    // we are introverts
+    // pictures of the backs of our heads
+  // future: 
+    // bios for us
+    // info about Q Tango
+// contact form
