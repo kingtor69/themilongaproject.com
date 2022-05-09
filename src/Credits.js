e6dfa472d73acf6548d8d6c226a505241722f9b4
Tor Kingdon <tor@hearkitty.com>
2022-05-09 15:19:20 -0600

    credits video embedded

--- src/Credits.js
+++ src/Credits.js
@@ -5,6 +5,7 @@ const Credits = () => {
     return (
         <div className="Credits">
             <h2 className="section-header">Credits</h2>
+            <div style="padding:56.21% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/707971594?h=7ba705814a?autoplay=1&loop=1&badge=0&autopause=0&player_id=0&app_id=58479/embed" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
         </div>
     );
 };
