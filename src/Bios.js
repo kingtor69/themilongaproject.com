4c60ce86fed047f5f9a1d15f4e2b227922d8c38c
Tor Kingdon <tor@hearkitty.com>
2022-05-17 11:33:23 -0600

    updated bios page

--- src/Bios.js
+++ src/Bios.js
@@ -6,7 +6,17 @@ const Bios = () => {
         <div className="Bios">
             <h2 className="section-header">About the Creators</h2>
             <h3><b>Julie Reichert</b> <i>writer / director / producer</i></h3>
+            <p>Julie Reichert’s filmmaking career began as a co-writer of Cannon Film’s <i>Breakin’ 2 is Electric Boogaloo</i> (1984). She wrote, directed, and produced three short films: <i>Girls</i> (2002), <i>Pipes</i> (2005), and <i>Wings</i> (2005), and the independent feature film <i><a href="http://warriorwomanfilm.com">Warrior Woman</a></i> (2012). She is also a paper artist and an educator.</p>
+            <h3><b>Kathleen Edwards</b> <i>producer / sound designer</i></h3>
             <p></p>
+            <h3><b>Tor Kingdon</b> <i>producer / sound mixer</i></h3>
+            <p></p>
+            <h3><b>Bill Mohr</b> <i>title</i></h3>
+            <p></p>
+            <h3><b>Betsy</b> <i>title</i></h3>
+            <p></p>
+            <h3><b>CK Barlow</b> <i>composer</i></h3>
+            <p>CK Barlow produces music for television and film, with placements in more than 1,500 TV episodes as well as commercials and video games. Her indie-film credits include Julie Reichert’s <i>Warrior Woman</i>, <i>Pipes</i>, and <i>Girls</i>. CK’s work for TV/film evolved from her scores and sound designs for dozens of live theater and dance productions in New Mexico. </p>
         </div>
     );
 };
