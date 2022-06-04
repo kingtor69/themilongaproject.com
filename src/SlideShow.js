2bce796a1479a26bd4345c114d8aa10a12a5c1b1
Tor Kingdon <kingtor@gmail.com>
2022-06-04 01:53:08 -0600

    photo editing

--- src/SlideShow.js
+++ src/SlideShow.js
@@ -1,20 +1,11 @@
 import React, {useState} from 'react';
-// import ImageCard from './ImageCard';
+import ImageGallery from 'react-image-gallery';
+import 'react-slideshow-image/dist/styles.css';
 
 const SlideShow = ({images}) => {
-    // an idea to randomize the order.... that can happen later
-    const [orderedImages, setOrderedImages] = useState(images)
-    // const [n, setN] = useState(Math.floor(Math.random() * unused.length));
-
-    // // const [img, setImg] = useState(unused[n]);
-    // useEffect(chooseImage = () => {
-        
-    // })
-
 
     return (
         <div className="SlideShow">
-            
         </div>
     );
 };
--- src/imageRefs.js
+++ src/imageRefs.js
@@ -10,22 +10,38 @@ import title010Grad10 from './images/Milonga010Grad10.png';
 import title020Grad10 from './images/Milonga020Grad10.png';
 import title100Grad10 from './images/Milonga100Grad10.png';
 import hearBettyLogo from './images/HearBettyLogoWithWords.jpg';
-import HearKittyADREmpty from './images/HearKittyADREmpty.jpg';
-import ErskineBando from './images/ErskineBando.jpg';
-import TorJulieRecordingQTango from './images/TorJulieRecordingQTango.jpg';
-import ElDiaSheetMusic from './images/ElDiaSheetMusic.jpg';
-import tangoSheetMusic from './images/tangoSheetMusic.jpg';
-import QTangoRecording from './images/QTangoRecording.jpg';
-import RumiRecording from './images/RumiRecording.jpg';
-import I25PondTurtles from './images/I25PondTurtles.jpg';
 
-import BradEditing from './images/BradEditing.jpg';
-import heyChief from './images/heyChiefGreenscreenWithJulie.jpg';
-import LibbyHelpingHandsGreenscreen from './images/LibbyHelpingHandsGreenscreen.jpg';
-// import JulieSlate105 from './images/JulieSlate105.jpg';
-import CodyJulieBottleCatGreenscreen from './images/CodyJulieBottleCatGreenscreen.jpg';
-import friendSlateLibbyWaiting from './images/friendSlateLibbyWaiting.jpg';
-import MiguelJulieGreenscreen from './images/friendSlateLibbyWaiting.jpg'
+import HearKittyADREmpty from './images/BTSoriginals/HearKittyADREmpty.png';
+import ErskineBando from './images/BTSoriginals/ErskineBando.png';
+import TorJulieRecordingQTango from './images/BTSoriginals/TorJulieRecordingQTango.png';
+import ElDiaSheetMusic from './images/BTSoriginals/ElDiaSheetMusic.png';
+import tangoSheetMusic from './images/BTSoriginals/tangoSheetMusic.png';
+import QTangoRecording from './images/BTSoriginals/QTangoRecording.png';
+import RumiRecording from './images/BTSoriginals/RumiRecording.png';
+import I25PondTurtles from './images/BTSoriginals/I25PondTurtles.png';
+import BradEditing from './images/BTSoriginals/BradEditing.png';
+import heyChief from './images/BTSoriginals/heyChiefGreenscreenWithJulie.png';
+import LibbyHelpingHandsGreenscreen from './images/BTSoriginals/LibbyHelpingHandsGreenscreen.png';
+// import JulieSlate105 from './images/BTSoriginals/JulieSlate105.png';
+import CodyJulieBottleCatGreenscreen from './images/BTSoriginals/CodyJulieBottleCatGreenscreen.png';
+import friendSlateLibbyWaiting from './images/BTSoriginals/friendSlateLibbyWaiting.png';
+import MiguelJulieGreenscreen from './images/BTSoriginals/friendSlateLibbyWaiting.png'
+
+import HearKittyADREmpty from './images/BTSthumbs/HearKittyADREmpty-thumb.png';
+import ErskineBando from './images/BTSthumbs/ErskineBando-thumb.png';
+import TorJulieRecordingQTango from './images/BTSthumbs/TorJulieRecordingQTango-thumb.png';
+import ElDiaSheetMusic from './images/BTSthumbs/ElDiaSheetMusic-thumb.png';
+import tangoSheetMusic from './images/BTSthumbs/tangoSheetMusic-thumb.png';
+import QTangoRecording from './images/BTSthumbs/QTangoRecording-thumb.png';
+import RumiRecording from './images/BTSthumbs/RumiRecording-thumb.png';
+import I25PondTurtles from './images/BTSthumbs/I25PondTurtles-thumb.png';
+import BradEditing from './images/BTSthumbs/BradEditing-thumb.png';
+import heyChief from './images/BTSthumbs/heyChiefGreenscreenWithJulie-thumb.png';
+import LibbyHelpingHandsGreenscreen from './images/BTSthumbs/LibbyHelpingHandsGreenscreen-thumb.png';
+// import JulieSlate105 from './images/BTSthumbs/JulieSlate105-thumb.png';
+import CodyJulieBottleCatGreenscreen from './images/BTSthumbs/CodyJulieBottleCatGreenscreen-thumb.png';
+import friendSlateLibbyWaiting from './images/BTSthumbs/friendSlateLibbyWaiting-thumb.png';
+import MiguelJulieGreenscreen from './images/BTSthumbs/friendSlateLibbyWaiting.png'
 
 const imageRefs = {
   camp,
@@ -38,20 +54,20 @@ const imageRefs = {
   title100Grad10,
   hearBettyLogo,
   behindTheScenesPics: [
-    { id: uuid(), image: BradEditing},
-    { id: uuid(), image: heyChief},
-    { id: uuid(), image: LibbyHelpingHandsGreenscreen},
-    { id: uuid(), image: CodyJulieBottleCatGreenscreen},
-    { id: uuid(), image: friendSlateLibbyWaiting},
-    { id: uuid(), image: MiguelJulieGreenscreen},
-    { id: uuid(), image: HearKittyADREmpty},
-    { id: uuid(), image: ErskineBando}, 
-    { id: uuid(), image: TorJulieRecordingQTango}, 
-    { id: uuid(), image: ElDiaSheetMusic}, 
-    { id: uuid(), image: tangoSheetMusic}, 
-    { id: uuid(), image: QTangoRecording}, 
-    { id: uuid(), image: RumiRecording}, 
-    { id: uuid(), image: I25PondTurtles}, 
+    { id: uuid(), original: BradEditing, description: 'Brad at work editing picture.'},
+    { id: uuid(), original: heyChief, description: 'Willis (Fighting Guy) setting up with Julie for his fight with Cody'},
+    { id: uuid(), original: LibbyHelpingHandsGreenscreen, description: 'Pip (Libby) at Helping Hands.'},
+    { id: uuid(), original: CodyJulieBottleCatGreenscreen, description: `Julie handing Wambli (Cody) his bottle of "hooch." (Kitty didn't make the cut.)`},
+    { id: uuid(), original: friendSlateLibbyWaiting, description: 'helping keep the photos organized'},
+    { id: uuid(), original: MiguelJulieGreenscreen, description: 'Julie with Pablo (Miguel)'},
+    { id: uuid(), original: HearKittyADREmpty, description: 'Hear Kitty, where we recorded most of the voices'},
+    { id: uuid(), original: ErskineBando, description: 'Erskine playing bandoneon during our recording session with Q Tango'}, 
+    { id: uuid(), original: TorJulieRecordingQTango, description: 'Tor & Julie during the Q Tango recording session'}, 
+    { id: uuid(), original: ElDiaSheetMusic, description: 'sheet music for a song heard frequently'}, 
+    { id: uuid(), original: tangoSheetMusic, description: 'more sheet music'}, 
+    { id: uuid(), original: QTangoRecording, description: 'The whole Q Tango ensemble'}, 
+    { id: uuid(), original: RumiRecording, description: 'Rumi recording as Laura'}, 
+    { id: uuid(), original: I25PondTurtles, description: 'Waiting for recording at Hear Kitty can be quite relaxing'}, 
   ],
 }
 
