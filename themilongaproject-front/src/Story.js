2b19080de0705e76524a4805a61d745d747dc1dd
Tor Kingdon <tor@hearkitty.com>
2022-10-13 17:03:46 -0600

    what's a milonga

--- themilongaproject-front/src/Story.js
+++ themilongaproject-front/src/Story.js
@@ -11,6 +11,13 @@ function Story() {
                 Libby has been living rough on the streets and sleeping in the woods for five years, drinking, not drinking, selling her handmade baskets, eating at the soup kitchen where she interacts with no one, trying to blot out the agonizing event that jettisoned her into a living death.  She resists the efforts of her A.A. sponsor, Pilar, to pierce her numbness, until one night, drunk and dumpster diving, she is seduced by tango music that draws her in despite herself.  Told through a unique combination of still photographs, painted backgrounds, narration, sound, and music, the story explores the complexities of support and betrayal, despair and hope, and the rocky road of the the human spirit.
             </p> 
         </div>
+        <br />
+        <div className="block text">
+          <h4>What’s a Milonga?</h4>
+          <p classname="Process-p-list-intro">
+            Milonga is a type of tango music, and also the name for an event where people dance tango and socialize. We are used to seeing performance tango on TV, with lots of choreographed drama and intricate steps, but at social dances — milongas — it’s more about the relationships between music and dancers, between dancers and their partners, and among the couples as they move around the dance floor. The dances are improvised, a conversation between partners, and you can have a great dance with some very simple steps. The music is passionate and haunting, Argentina’s version of the blues, often with similar kinds of lyrics about pain, loss and betrayal. An overall theme is that life is often harsh but you shrug your shoulders and dance.
+        </p>
+      </div>
     </div>
   );
 }
